5f987c1fbacd4067664cd60cc242ac240e1c1a5b
Hanna Halka <hanna.dm.galka@gmail.com>
2025-06-05 04:12:26 +0300

    print file columns

---
+++ file_processing.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+df_2019 = pd.read_csv('data/2019.csv', encoding='cp1251', low_memory=False, sep=';')
+print(df_2019.columns.tolist())
+
+df_2020 = pd.read_csv('data/2020.csv', encoding='1251', low_memory=False, sep=';')
+print(df_2020.columns.tolist())
+
+df_2021 = pd.read_csv('data/2021.csv', encoding='', low_memory=False, sep=';')
+print(df_2021.columns.tolist())
+
+df_2022 = pd.read_csv('data/2022.csv', encoding='', sep=';')
+print(df_2022.columns.tolist())
+
+df_2023 = pd.read_csv('data/2023.csv', encoding='', sep=';')
+print(df_2023.columns.tolist())
+
+df_2024 = pd.read_csv('data/2024.csv', encoding='', sep=';')
+print(df_2024.columns.tolist())
