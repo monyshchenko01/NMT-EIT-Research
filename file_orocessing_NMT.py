fcebdc42e90ca3fe288289c08b9c163918c65132
Hanna Halka <hanna.dm.galka@gmail.com>
2025-06-06 14:04:23 +0300

    renaming columns

---
+++ file_orocessing_NMT.py
@@ -0,0 +1,49 @@
+import pandas as pd
+
+
+def select_col(df):
+    ball100 = []
+    for col in df.columns:
+        if 'Ball100' in col:
+            ball100.append(col)
+        if col == 'outid':
+            df = df.rename(columns={'outid': 'OUTID'})
+        if col == 'SEXTYPENAME':
+            df = df.rename(columns={'SEXTYPENAME': 'Sex'})
+        if col == 'SexTypeName':
+            df = df.rename(columns={'SexTypeName': 'Sex'})
+
+    df = df[['OUTID', 'Sex', 'Age'] + ball100]
+
+    df = df.rename(columns={'UkrBlockBall100': 'Ukrainian'})
+    df = df.rename(columns={'HistBlockBall100': 'History'})
+    df = df.rename(columns={'MathBlockBall100': 'Math'})
+    df = df.rename(columns={'PhysBlockBall100': 'Physic'})
+    df = df.rename(columns={'ChemBlockBall100': 'Chemistry'})
+    df = df.rename(columns={'BioBlockBall100': 'Biology'})
+    df = df.rename(columns={'GeoBlockBall100': 'Geography'})
+    df = df.rename(columns={'EngBlockBall100': 'English'})
+    df = df.rename(columns={'FraBlockBall100': 'French'})
+    df = df.rename(columns={'DeuBlockBall100': 'German'})
+    df = df.rename(columns={'SpaBlockBall100': 'Spanish'})
+    df = df.rename(columns={'UkrLitBlockBall100': 'Ukrainian Literature'})
+
+    return df
+
+
+
+df_2022 = pd.read_csv('data/2022.csv', encoding='', sep=';')
+df_2022['Age'] = 2022 - df_2022['Birth']
+df_2022 = select_col(df_2022)
+
+df_2023 = pd.read_csv('data/2023.csv', encoding='', sep=';')
+df_2023['Age'] = 2023 - df_2023['Birth']
+df_2023 = select_col(df_2023)
+
+df_2024 = pd.read_csv('data/2024.csv', encoding='', sep=';')
+df_2024['Age'] = 2024 - df_2024['Birth']
+df_2024 = select_col(df_2024)
+
+NMT = pd.concat([df_2022, df_2023, df_2024], ignore_index=True)
+
+# print(NMT.columns)
---
+++ file_processing_EIT.py
@@ -0,0 +1,57 @@
+import pandas as pd
+
+
+def select_col(df):
+    ball100 = []
+    for col in df.columns:
+        if 'Ball100' in col:
+            ball100.append(col)
+        if col == 'SEXTYPENAME':
+            df = df.rename(columns={'SEXTYPENAME': 'Sex'})
+        if col == 'SexTypeName':
+            df = df.rename(columns={'SexTypeName': 'Sex'})
+
+    df = df[['OUTID', 'Sex', 'Age'] + ball100]
+
+    df = df.rename(columns={'UkrBall100': 'Ukrainian'})
+    df = df.rename(columns={'histBall100': 'History'})
+    df = df.rename(columns={'mathBall100': 'Math'})
+    df = df.rename(columns={'physBall100': 'Physic'})
+    df = df.rename(columns={'chemBall100': 'Chemistry'})
+    df = df.rename(columns={'bioBall100': 'Biology'})
+    df = df.rename(columns={'geoBall100': 'Geography'})
+    df = df.rename(columns={'engBall100': 'English'})
+    df = df.rename(columns={'fraBall100': 'French'})
+    df = df.rename(columns={'deuBall100': 'German'})
+    df = df.rename(columns={'spaBall100': 'Spanish'})
+
+    df = df.rename(columns={'UMLBall100': 'Ukrainian Literature'})
+    df = df.rename(columns={'HistBall100': 'Applied History'})
+    df = df.rename(columns={'MathBall100': 'Applied Math'})
+    df = df.rename(columns={'PhysBall100': 'Applied Physic'})
+    df = df.rename(columns={'ChemBall100': 'Applied Chemistry'})
+    df = df.rename(columns={'BioBall100': 'Applied Biology'})
+    df = df.rename(columns={'GeoBall100': 'Applied Geography'})
+    df = df.rename(columns={'EngBall100': 'Applied English'})
+    df = df.rename(columns={'FraBall100': 'Applied French'})
+    df = df.rename(columns={'DeuBall100': 'Applied German'})
+    df = df.rename(columns={'SpaBall100': 'Applied Spanish'})
+
+    return df
+
+
+df_2019 = pd.read_csv('data/2019.csv', encoding='cp1251', low_memory=False, sep=';')
+df_2019['Age'] = 2019 - df_2019['Birth']
+df_2019 = select_col(df_2019)
+
+df_2020 = pd.read_csv('data/2020.csv', encoding='1251', low_memory=False, sep=';')
+df_2020['Age'] = 2020 - df_2020['Birth']
+df_2020 = select_col(df_2020)
+
+df_2021 = pd.read_csv('data/2021.csv', encoding='', low_memory=False, sep=';')
+df_2021['Age'] = 2021 - df_2021['Birth']
+df_2021 = select_col(df_2021)
+
+EIT = pd.concat([df_2019, df_2020, df_2021], ignore_index=True)
+
+# print(EIT.columns)
