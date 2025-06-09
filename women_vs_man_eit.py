b78f627d82f14f6be60613fe9b8041e6f698be69
Hanna Halka <hanna.dm.galka@gmail.com>
2025-06-09 14:25:08 +0300

    print results

--- women_vs_man_eit.py
+++ women_vs_man_eit.py
@@ -8,4 +8,9 @@ men = eit[eit['Sex'] == 'чоловіча']
 avg_women = women['Average Score'].mean()
 avg_men = men['Average Score'].mean()
 
-print(avg_women, '\n', avg_men)
+print(f"Average woman score on EIT:\t{avg_women.round()}\nAverage man score on EIT:\t{avg_men.round()}\n\n")
+
+avg_women_by_subject = women[file_processing_EIT.grades_only].mean()
+avg_men_by_subject = men[file_processing_EIT.grades_only].mean()
+
+print(f"Average woman score on EIT by subject: \n{avg_women_by_subject.round()}\n\nAverage man score on EIT by subject: \n{avg_men_by_subject.round()}")
--- women_vs_man_nmt.py
+++ women_vs_man_nmt.py
@@ -8,4 +8,9 @@ men = nmt[nmt['Sex'] == 'чоловіча']
 avg_women = women['Average Score'].mean()
 avg_men = men['Average Score'].mean()
 
-print(avg_women, '\n', avg_men)
+print(f"Average woman score on NMT:\t{avg_women.round()}\nAverage man score on NMT:\t{avg_men.round()}\n\n")
+
+avg_women_by_subject = women[file_processing_NMT.grades_only].mean()
+avg_men_by_subject = men[file_processing_NMT.grades_only].mean()
+
+print(f"Average woman score on NMT by subject: \n{avg_women_by_subject.round()}\n\nAverage man score on NMT by subject: \n{avg_men_by_subject.round()}")
