7397a7935955efd3596b79a54bd65a0f194e3158
Hanna Halka <hanna.dm.galka@gmail.com>
2025-06-09 13:36:26 +0300

    nmt/eit women vs men

--- women_vs_man_eit.py
+++ women_vs_man_eit.py
@@ -1,15 +1,11 @@
-import pandas as pd
 import file_processing_EIT
 
 eit = file_processing_EIT.EIT
 
-only_scors = eit.columns.difference(['Sex', 'Age', 'OUTID'])
-eit[only_scors] = eit[only_scors].apply(pd.to_numeric, errors='coerce')
+women = eit[eit['Sex'] == 'жіноча']
+men = eit[eit['Sex'] == 'чоловіча']
 
-women = eit[eit['Sex'] == 'жінка']
-men = eit[eit['Sex'] == 'чоловік']
+avg_women = women['Average Score'].mean()
+avg_men = men['Average Score'].mean()
 
-avg_women = women[only_scors].mean()
-avg_men = men[only_scors].mean()
-
-# print(avg_women, '\n', avg_men)
+print(avg_women, '\n', avg_men)
--- women_vs_man_nmt.py
+++ women_vs_man_nmt.py
@@ -1,15 +1,11 @@
-import pandas as pd
 import file_processing_NMT
 
 nmt = file_processing_NMT.NMT
 
-only_scors = nmt.columns.drop(['Sex', 'Age', 'OUTID'])
+women = nmt[nmt['Sex'] == 'жіноча']
+men = nmt[nmt['Sex'] == 'чоловіча']
 
-women = nmt[nmt['Sex'] == 'жінка']
-men = nmt[nmt['Sex'] == 'чоловік']
+avg_women = women['Average Score'].mean()
+avg_men = men['Average Score'].mean()
 
-avg_women = women[only_scors].mean()
-avg_men = men[only_scors].mean()
-
-# print(avg_women, '\n', avg_men)
-print(nmt[only_scors].values.tolist())
+print(avg_women, '\n', avg_men)
