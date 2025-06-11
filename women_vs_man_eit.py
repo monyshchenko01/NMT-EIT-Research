da7ac8f43d57e9e7fb1a71bcea1ef0bab03209ff
Hanna Halka <hanna.dm.galka@gmail.com>
2025-06-11 18:26:15 +0300

    women vs men visualisation

--- women_vs_man_eit.py
+++ women_vs_man_eit.py
@@ -8,9 +8,9 @@ men = eit[eit['Sex'] == 'чоловіча']
 median_women = women['Average Score'].median()
 median_men = men['Average Score'].median()
 
-print(f"Average woman score on EIT:\t{median_women.round()}\nAverage man score on EIT:\t{median_men.round()}\n\n")
+print(f"Average women score on EIT:\t{median_women.round()}\nAverage men score on EIT:\t{median_men.round()}\n\n")
 
 median_women_by_subject = women[file_processing_EIT.grades_only].median()
 median_men_by_subject = men[file_processing_EIT.grades_only].median()
 
-print(f"Average woman score on EIT by subject: \n{median_women_by_subject.round()}\n\nAverage man score on EIT by subject: \n{median_men_by_subject.round()}")
+print(f"Average women score on EIT by subject: \n{median_women_by_subject.round()}\n\nAverage men score on EIT by subject: \n{median_men_by_subject.round()}")
--- women_vs_man_nmt.py
+++ women_vs_man_nmt.py
@@ -8,9 +8,9 @@ men = nmt[nmt['Sex'] == 'чоловіча']
 median_women = women['Average Score'].median()
 median_men = men['Average Score'].median()
 
-print(f"Average woman score on NMT:\t{median_women.round()}\nAverage man score on NMT:\t{median_men.round()}\n\n")
+print(f"Average women score on NMT:\t{median_women.round()}\nAverage men score on NMT:\t{median_men.round()}\n\n")
 
 median_women_by_subject = women[file_processing_NMT.grades_only].median()
 median_men_by_subject = men[file_processing_NMT.grades_only].median()
 
-print(f"Average woman score on NMT by subject: \n{median_women_by_subject.round()}\n\nAverage man score on NMT by subject: \n{median_men_by_subject.round()}")
+print(f"Average women score on NMT by subject: \n{median_women_by_subject.round()}\n\nAverage men score on NMT by subject: \n{median_men_by_subject.round()}")
